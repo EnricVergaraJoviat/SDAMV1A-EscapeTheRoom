0d5daf0d614bd638b532655a011e817814a4c7f5
Enric Vergara <donkikochan@gmail.com>
2023-11-20 09:39:06 +0100

    pujant els elements comuns

--- Assets/Scripts/RoomInfo.cs
+++ Assets/Scripts/RoomInfo.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class RoomInfo
 {
+    public RoomInfo(float time, int tries)
+    {
+        this.time = time;
+        this.tries = tries;
+    }
     public float time;
     public int tries;
     // Start is called before the first frame update
