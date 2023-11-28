ffe4d3000a884f95c40c1aee49627fd7af738647
Enric Vergara <donkikochan@gmail.com>
2023-11-28 18:59:47 +0100

    working on roomManager

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -15,6 +15,11 @@ public class RoomManager : MonoBehaviour
     public TMP_InputField inputF_Solution;
     public GameObject panelRoomSolved;
     public GameObject[] panelToHideWithVictory;
+    public TextMeshProUGUI labelTries;
+    public AudioClip[] audio_code_correct;
+    public AudioClip audio_code_wrong;
+    public AudioClip audio_next;
+    public AudioSource audioSource;
     
     //---Setup custom room:
     public int solutionCode;
@@ -94,6 +99,7 @@ public class RoomManager : MonoBehaviour
 
     public void CheckSolution()
     {
+        audioSource.PlayOneShot(audio_next);
         if (inputF_Solution.text == this.solutionCode.ToString())
         {
             foreach (var o in panelToHideWithVictory)
@@ -101,6 +107,12 @@ public class RoomManager : MonoBehaviour
                 o.SetActive(false);
             }
             Debug.Log("The CODE is Correct!");
+            
+            int randomIndex = Random.Range(0, audio_code_correct.Length);
+            AudioClip randomClip = audio_code_correct[randomIndex];
+            audioSource.clip = randomClip;
+            audioSource.Play();
+            
             panelRoomSolved.SetActive(true);
             roomSolved = true;
             panelRoomSolved.SetActive(true);
@@ -112,7 +124,11 @@ public class RoomManager : MonoBehaviour
         else
         {
             tries++;
+            labelTries.text ="" + tries;
+            labelTries.color = Color.red;
             Debug.Log("The CODE is Wrong!");
+            audioSource.clip = audio_code_wrong;
+            audioSource.Play();
         }
     }
 
